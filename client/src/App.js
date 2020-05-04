b24f3e3cf1a6b154e7d16ea3cc6366859c778c00
Diego Reyes <reyesdiego@hotmail.com>
2020-05-04 01:32:45 -0300

    Bug fixing installing antd - moment version issue - React client skeleton

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Table } from 'antd';
+import 'antd/dist/antd.css'; //TODO move to index.html
+
+
+function App() {
+  return (
+    <div className="App">
+      <Table></Table>
+    </div>
+  );
+}
+
+export default App;
