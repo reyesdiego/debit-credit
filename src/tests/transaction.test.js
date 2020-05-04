b3a2122a45f7ba21791f7cf1c2f922870b51254d
Diego Reyes <reyesdiego@hotmail.com>
2020-05-03 22:00:16 -0300

    First Commit

---
+++ src/tests/transaction.test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+describe('TRANSACTIONS', function () {
+    it('Should Pass (dummy test)', function () {
+        expect(1).to.eq(1);
+    })
+    it('should insert transaction', function () {
+
+    })
+});
