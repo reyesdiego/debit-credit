46250e962d8323398816605b026292fd878e8903
Diego Reyes <reyesdiego@hotmail.com>
2020-05-03 22:10:36 -0300

    Start creating transaction service

--- src/tests/transaction.test.js
+++ src/tests/transaction.test.js
@@ -1,9 +1,12 @@
 const expect = require('chai').expect;
+
+const createTransactionEngine = require('../services/transaction');
+
 describe('TRANSACTIONS', function () {
     it('Should Pass (dummy test)', function () {
         expect(1).to.eq(1);
     })
-    it('should insert transaction', function () {
-
+    it('should insert Debit Transaction', function () {
+        const transactionEngine = createTransactionEngine();
     })
 });
